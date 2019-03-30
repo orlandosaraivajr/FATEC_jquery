9d307f7142c26580ec33310784bfb2b75fc8ce14
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2019-03-30 11:49:36 -0300

    Exercícios em sala

--- jquery/Aula 03/app.js
+++ jquery/Aula 03/app.js
@@ -1,7 +1,23 @@
 var main = function() {
-//    $("*").css("color","red");
-    $("h1").css("color","red");
+//  $("*").css("color","red");
+//	$(".important").css("color","red");
+//	$(".relevant p:first").css("color","red");
+//  $(".relevant p:eq(2)").css("color","red");
+//  $("p").css("color","red");
+//  $(".relevant p").css("color","red");
+//  $(".relevant p:even").css("color","red");
+//  $(".relevant p:odd").css("color","red");
+//  $(".relevant p:last").css("color","red");
+//  $(".relevant p:eq(-1)").css("color","red");
+/*
+  $(".relevant p").each(function(index, el){
+	  if(index>3) {
+		  el.style = 'color:red'
+	  }
+  })
+*/
+// $(".relevant p:gt(3)").css("color","red");
+// $(".relevant p:not(.a)").css("color","red");
 };
 
 jQuery(document).ready(main);
-
